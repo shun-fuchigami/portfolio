f223188f2a931bdbc4153f2d3ec3017e03a84801
shun-fuchigami <79799090+shun-fuchigami@users.noreply.github.com>
2022-03-30 12:55:56 +0900

    タイルを表示

--- src/sketch.js
+++ src/sketch.js
@@ -1,19 +1,24 @@
 // setup時に読み込んだ画像を保持
-const TileImages=[];
-
+const tileImages=[];
+let a;
 // 本処理
 export const sketch = (p)=>{
   
   // 読み込み時の1番初めに実行
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    tileImages.push(p.loadImage("./imgs/grass.png"));
   }
   
   // setup後の繰り返し処理
+  console.log(p.height);
   p.draw = () => {
-    p.background('#0f2350');
-    p.noStroke();
-    p.fill(255);
-    p.ellipse(p.windowWidth/2, p.windowHeight/2, 50, 50);
+    p.background('#485859');
+    p.image(tileImages[0],1,1);
+    if(p.windowWidth !== p.width || p.windowHeight!==p.height){
+      p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+      }
+    }
   }
 };
